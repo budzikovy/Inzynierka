05d3ab2284f9d676934af81a6ffd270bcd98e3c3
Krzysztof Dąbrowski <80182@student.pb.edu.pl>
2023-11-26 22:11:10 +0100

    Api_Stock
    Działa

---
+++ Controllers/StockController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Inz_Fn.Models;
+
+namespace Inz_Fn.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StockController : Controller
+    {
+        public async Task<IActionResult> Stock()
+        {
+            List<Stock_model> stockModels = await GetStockData();
+            return View(stockModels);
+        }
+
+
+        private async Task<List<Stock_model>> GetStockData()
+        {
+            string apiKey = "TuP9o6bqsfqxilONFO1cVhApCcvy7wTR";
+            string symbol = "AAPL";
+            string apiUrl = $"https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/day/2021-01-01/2021-12-31?apiKey={apiKey}";
+
+            using HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(content);
+                JToken results = json["results"];
+
+                List<Stock_model> stockModels = new List<Stock_model>();
+                foreach (JToken result in results)
+                {
+                    Stock_model stockModel = result.ToObject<Stock_model>();
+                    stockModels.Add(stockModel);
+                }
+
+                return stockModels;
+            }
+            else
+            {
+                throw new Exception($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
---
+++ Models/Stock_model.cs
@@ -0,0 +1,20 @@
+﻿namespace Inz_Fn.Models
+{
+    public class Stock_model
+    { 
+        public double c {  get; set; }
+        public double h { get; set; }
+        public double l { get; set; }
+        public int n { get; set; }
+        public double o { get; set; }
+        public Boolean otc { get; set; }
+        public long t { get; set; }
+        public long v { get; set; }
+        public double vw { get; set; }
+
+    }
+    public class ApiResponse
+    {
+        public List<Stock_model> Stock_models { get; set; }
+    }
+}
